1f84d8598cedb2d11b1f72984a8487c6b4387e50
d3adc3ii <d3social@pm.me>
2024-10-06 14:47:03 +0800

    test

--- files.tf
+++ files.tf
@@ -9,7 +9,7 @@ resource "proxmox_virtual_environment_download_file" "talos_nocloud_image" {
   datastore_id            = "local"
   node_name               = var.talos-common.node_name
 
-  file_name               = "talos-${local.talos.version}-nocloud-amd64.img"
+  file_name               = "talos-${local.talos.version}-nocloud-amd64.iso"
   #url                     = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/${local.talos.version}/nocloud-amd64.iso" 
   url                     = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/v1.8.0/nocloud-amd64.iso"
   #decompression_algorithm = "gz"
